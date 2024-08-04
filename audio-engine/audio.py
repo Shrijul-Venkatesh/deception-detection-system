48176aa4b43d2bd075511ce84de7519c4c5b1a22
Shrijul-Venkatesh <venkateshshrijul@gmail.com>
2024-08-05 00:00:31 +0530

    re-organizing directories

--- audio-engine/audio.py
+++ audio-engine/audio.py
@@ -2,10 +2,10 @@ import matlab.engine as Engine
 
 engine = Engine.start_matlab()
 
-matlab_functions_dir = r"./Deception_Detection_System/Audio_Engine/MATLAB"
+matlab_functions_dir = r"./MATLAB"
 engine.addpath(matlab_functions_dir, nargout=0)
 
-audio_file_path = r"./Dataset/MU3D-Package/audio_set/BF001_1PT.mp3"
+audio_file_path = r"../../MU3D-Package/audio_set/BF001_1PT.mp3"
 
 mfccs = engine.mfcc_extraction(audio_file_path)
 mfccs = [list(row) for row in mfccs]
